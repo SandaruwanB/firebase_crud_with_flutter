cd9f71bf8f8863a4972d8f260c96858a5b47feb3
SandaruwanB <sandarusbandara110@gmail.com>
2024-08-27 20:40:11 +0530

    [DEV] employee add classes

---
+++ lib/screens/employees_add.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class EmployeesAdd extends StatefulWidget {
+  const EmployeesAdd({super.key});
+
+  @override
+  State<EmployeesAdd> createState() => _EmployeeAddState();
+}
+
+class _EmployeeAddState extends State<EmployeesAdd> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget build(BuildContext contex) {
+    return MaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
+      debugShowCheckedModeBanner: false,
+      title: "Add Employees",
+      home: const Scaffold(),
+    );
+  }
+}
