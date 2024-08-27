2dec41a9cb48a323c4fe15090caa36abde98fdfb
SandaruwanB <sandarusbandara110@gmail.com>
2024-08-27 20:18:58 +0530

    [DEV] employee page and firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_test/screens/employees.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const Employees());
 }
