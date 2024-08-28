9960d165e7c87d728bc8fbe6214566b9328a1338
SandaruwanB <sandarusbandara110@gmail.com>
2024-08-28 10:59:01 +0530

    [DEV] grid

--- lib/screens/employees.dart
+++ lib/screens/employees.dart
@@ -31,13 +31,24 @@ class _EmployeesState extends State<Employees> {
           backgroundColor: Colors.blue,
           title: const Text("Employees"),
         ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          children: List.generate(100, (index) {
-            return Center(
+        body: GridView.builder(
+          itemCount: 1000,
+          padding: const EdgeInsets.all(4.0),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 2.0,
+            mainAxisSpacing: 4.0,
+            crossAxisSpacing: 4.0,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                color: Colors.grey,
+              ),
               child: Text("Item $index"),
             );
-          }),
+          },
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: navigateToForm,
